c056beaac69f7e107e8c9121b3990e1057452b20
russell <russell.politzky@dariel.co.za>
2012-08-31 17:55:28 +0200

    Added test to show how to create private state for Ember mixins.

--- emberTests.js
+++ emberTests.js
@@ -133,4 +133,88 @@ $(document).ready(function () {
         var instance2InitialValue2 = instance2.functionThatUsesPrivateInstanceState();
         equal(instance2InitialValue2, 99, 'Expected initial value to be 30 but was ' + instance2InitialValue2);
     });
+
+
+    // This test shows how to create private state for Ember Mixins.
+    // Ordinarily, it is assumed that Ember Mixins consist of stateless,
+    // public functions and that there's no shared private state
+    // between them.  This limitation can be overcome by building
+    // a mixin contructor as shown below.  The constructor is based
+    // on an immediate function which returns an initializer object
+    // for the mixin.  The initializer object uses a closure to 
+    // access private state.  A factory function creates a new mixin 
+    // for every call to achieve private instance state.
+    //
+    // In addition is is possibe to create private static state 
+    // for mixins.  This is achieved by using a second, immediate
+    // function which is only executed once.
+    test('should be able to create private instance methods in an Ember mixin', function () {
+        var messageForPrivateStaticFunction = 'I am a privateStaticFunction';
+
+        var mixinConstructor = (function () { // This immediate function creates the static state.
+            var privateStaticState = 10;
+            var privateStaticFunction = function () { return messageForPrivateStaticFunction; };
+
+            return function (mixinConstructorParameter) {
+
+                return Ember.Mixin.create(
+                // In a standard mixin, this is only executed once.
+                // which is why we need a factory function to build 
+                // a new one for each instance of the mixin.
+                    (function () {
+                        var privateFunction = function () {
+                        };
+                        var privateState = mixinConstructorParameter;
+                        return {
+                            privateFunction: function () { return privateFunction; }, // Working with private functions.
+                            privateState: privateState,  // Working with private state.
+                            propertyBasedOnStaticState: function (key, value) { // Note that we need to use computed property here.
+                                if (arguments.length === 1) { // getter
+                                    return privateStaticState;
+                                } else { // setter
+                                    privateStaticState = value;
+                                    return value;
+                                }
+                            } .property(),
+                            usesPrivateStaticFunction: function () { // Uses private static function
+                                return privateStaticFunction();
+                            }
+                        };
+                    })());
+            };
+        })();
+        
+        var parmater1 = 1;
+        var parmater2 = 2;
+
+        // Note that we use the mixin constructor to build a new instance with its own private state.
+        var emberObjectA = Ember.Object.create(mixinConstructor(parmater1), {});
+        var emberObjectB = Ember.Object.create(mixinConstructor(parmater2), {});
+
+        var privateFunctionA = emberObjectA.get('privateFunction');
+        var privateFunctionB = emberObjectB.get('privateFunction');
+
+        ok(!(privateFunctionA === privateFunctionB), 'Expected that separate private instance members were created.');
+
+        var privateStateA = emberObjectA.get('privateState');
+        var privateStateB = emberObjectB.get('privateState');
+
+        ok(privateStateA === parmater1, 'Expected to get separate and private state from each mixin.');
+        ok(privateStateB === parmater2, 'Expected to get separate and private state from each mixin.');
+
+        // Test private statics in the mixin.
+        var valueOfStaticState = emberObjectA.get('propertyBasedOnStaticState');
+        emberObjectA.set('propertyBasedOnStaticState', 12);
+
+        var newValueOfStaticStateA = emberObjectA.get('propertyBasedOnStaticState');
+        var newValueOfStaticStateB = emberObjectB.get('propertyBasedOnStaticState');
+        equal(newValueOfStaticStateA, newValueOfStaticStateB, 'Expected static state to be the same from all objects.');
+
+        var resultFromPrivateStaticFunctionOnA = emberObjectA.usesPrivateStaticFunction();
+        var resultFromPrivateStaticFunctionOnB = emberObjectB.usesPrivateStaticFunction();
+
+        equal(resultFromPrivateStaticFunctionOnA, messageForPrivateStaticFunction, 'Expected correct message from private static function.');
+        equal(resultFromPrivateStaticFunctionOnB, messageForPrivateStaticFunction, 'Expected correct message from private static function.');
+    });
+    
 });
